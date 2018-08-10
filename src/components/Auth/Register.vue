8e1400d4f3a99b4dde3839b9b80e9a765b1c824b
Denys <denys@Admins-iMac.local>
2018-08-10 15:55:10 +0200

    add files

---
+++ src/components/Auth/Register.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Registration form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                  prepend-icon="person"
+                  name="email"
+                  label="Email"
+                  type="email"
+                  :rules="emailRules"
+                  v-model="email"
+              ></v-text-field>
+              <v-text-field
+                  prepend-icon="lock"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  :counter="6"
+                  :rules="passwordRules"
+                  v-model="password"
+              ></v-text-field>
+              <v-text-field
+                  prepend-icon="repeat"
+                  name="confirm-password"
+                  label="Confirm Password"
+                  type="password"
+                  :counter="6"
+                  :rules="ConfirmPasswordRules"
+                  v-model="ConfirmPassword"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="primary"
+                @click="onSubmit"
+                :disabled="!valid"
+            >Create Account</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        email: '',
+        password: '',
+        ConfirmPassword: '',
+        ConfirmPasswordRules: [
+          v => !!v || 'Password is required',
+          v => v === this.password || 'Password should match'
+        ],
+        valid: false,
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+/.test(v) || 'E-mail must be valid'
+        ],
+        passwordRules: [
+          v => !!v || 'Password is required',
+          v => (v && v.length <= 6) || 'Password must be equal or 10 characters'
+        ]
+      }
+    },
+    methods: {
+      onSubmit () {
+        if (this.$refs.form.validate()) {
+          const user = {
+            email: this.email,
+            password: this.password
+          }
+          console.log(user)
+        }
+      }
+    }
+  }
+</script>
---
+++ src/components/User/Checkout.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex
+          xs12
+          sm6
+          ofset-sm3
+      >
+        <h1 class="text-secondary mb-3">Orders</h1>
+        <v-list
+            subheader
+            two-line
+        >
+          <v-list-tile
+              avatar
+              v-for="(order, i) in orders"
+              :key="i"
+          >
+            <v-list-tile-action>
+              <v-checkbox
+                  color="success"
+                  :input-value="order.done"
+                  @change="markDone(order)"
+              ></v-checkbox>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{order.name}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn class="primary"
+                     :to="'/product' + order.productId"
+              >Open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        orders: [
+          {
+            id: 'test',
+            name: 'Denys',
+            phone: '02',
+            productId: '1',
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      markDone (order) {
+        order.done = true
+      }
+    }
+  }
+</script>
+
