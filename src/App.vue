a9a735ee3f8596e90b3c5c0b7b961e194ff0668a
Denys <denys@Admins-iMac.local>
2018-08-13 10:17:00 +0200

    add errors

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <v-app>
     <Navbar/>
+    <template v-if="error">
+      <v-snackbar
+          :multi-line="true"
+          :timeout="5000"
+          color="error"
+          @input="closeError"
+          :value="true"
+      >
+        {{ error }}
+        <v-btn
+            color="pink"
+            flat
+            dark
+            @click.native="closeError"
+        >
+          Close
+        </v-btn>
+      </v-snackbar>
+    </template>
+
   </v-app>
 </template>
 
@@ -9,6 +29,16 @@
   export default {
     components: {
       Navbar
+    },
+    methods: {
+      closeError () {
+        this.$store.dispatch('clearError')
+      }
+    },
+    computed: {
+      error () {
+        return this.$store.getters.error
+      }
     }
   }
 </script>
