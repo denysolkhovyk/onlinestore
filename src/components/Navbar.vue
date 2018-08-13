ea05933ab37e52267e987964e7cdef8f4fa02412
Denys <denys@Admins-iMac.local>
2018-08-13 10:55:24 +0200

    add logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,6 +14,18 @@
             <v-list-tile-title v-text="link.title"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-list-tile
+            @click="onLogout"
+            v-if="isUserLoggedIn"
+        >
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title v-text="'Logout'"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app dark color="primary">
@@ -38,6 +50,13 @@
           <v-icon left>{{link.icon}}</v-icon>
           {{link.title}}
         </v-btn>
+        <v-btn flat
+               @click="onLogout"
+               v-if="isUserLoggedIn"
+        >
+          <v-icon left>exit_to_app</v-icon>
+          Logout
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
@@ -50,14 +69,32 @@
   export default {
     data () {
       return {
-        sideNav: false,
-        links: [
-          {title: 'Login', icon: 'account_box', url: '/login'},
-          {title: 'Register', icon: 'face', url: '/register'},
-          {title: 'Cart', icon: 'shopping_cart', url: '/checkout'},
-          {title: 'New Product', icon: 'add', url: '/new'},
-          {title: 'My Products', icon: 'list', url: '/list'}
-        ]
+        sideNav: false
+      }
+    },
+    methods: {
+      onLogout () {
+        this.$store.dispatch('logoutUser')
+        this.$router.push('/')
+      }
+    },
+    computed: {
+      isUserLoggedIn () {
+        return this.$store.getters.isUserLoggedIn
+      },
+      links () {
+        if (this.isUserLoggedIn) {
+          return [
+            {title: 'Cart', icon: 'shopping_cart', url: '/checkout'},
+            {title: 'New Product', icon: 'add', url: '/new'},
+            {title: 'My Products', icon: 'list', url: '/list'}
+          ]
+        } else {
+          return [
+            {title: 'Login', icon: 'account_box', url: '/login'},
+            {title: 'Register', icon: 'face', url: '/register'}
+          ]
+        }
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -27,5 +27,11 @@ new Vue({
       messagingSenderId: '206932177056'
     }
     fb.initializeApp(config)
+
+    fb.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$store.dispatch('autoLoginUser', user)
+      }
+    })
   }
 })
